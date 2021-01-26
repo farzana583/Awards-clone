08859b13ff5bfc00787dfeb2cf81875601faf157
farzana583 <farzanaisack@gmail.com>
2021-01-27 01:05:24 +0300

    modified  appawarrd/urls.py

--- appawarrd/urls.py
+++ appawarrd/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 urlpatterns=[  
      path('',views.home,name = 'home'),
      path('registration/login/',views.loginPage, name="login"),
      path('registration/registration_form.html/',views.registerPage, name="register"),
      path('logout/',views.logoutUser, name="logout"),
-     path('profile/', views.profile, name = 'profile'),
+     path('accounts/profile/', views.profile, name = 'profile'),
+     path('rate/<int:id>/',views.rate,name='rates'),
+     url(r'^singleproject/(\d+)',views.single_project,name='singleproject'),
      path('postImage/', views.new_post, name = 'post_image'),
 
      
